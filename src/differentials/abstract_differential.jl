f0a07d7bec07267ca3d326a0ea9fbf0434bc9445
Lyndon White <oxinabox@ucc.asn.au>
2020-01-13 17:52:14 +0000

    Fix typos
    Co-Authored-By: Nick Robinson <npr251@gmail.com>

--- src/differentials/abstract_differential.jl
+++ src/differentials/abstract_differential.jl
@@ -11,17 +11,17 @@ All subtypes of `AbstractDifferential` implement the following operations:
 
 `+(a, b)`: linearly combine differential `a` and differential `b`
 
-`*(a, b)`: multiply the differential `b` by the scalaring factor `b`
+`*(a, b)`: multiply the differential `b` by the scaling factor `b`
 
 `Base.conj(x)`: complex conjugate of the differential `x`
 
 `Base.zero(x) = Zero()`: a zero.
 
-In general a differential is the type of a derivative of a value.
+In general a differential type is the type of a derivative of a value.
 The type of the value is for contrast called the primal type.
 Differntial types correspond to primal types, though the relation is not one-to-one.
 Not only `AbstractDifferential` are valid differentials.
-Infact for the most common primal types, such as `Real` or `AbstractArray{Real}` the
+In fact for the most common primal types, such as `Real` or `AbstractArray{Real}` the
 the differential type is the same as the primal type.
 
 In a circular definition: the most important property of a differential is that it should
@@ -36,22 +36,22 @@ Base.:+(x::AbstractDifferential) = x
 """
     extern(x)
 
-Makes a best effort attempt to convert a differential, into a primal value.
+Makes a best effort attempt to convert a differential into a primal value.
 This is not always a well-defined operation.
 For two reasons:
  - It may not be possible to determine the primal type for a given differential.
  For example `Zero` is a valid differential for any primal.
  - The primal type might not be a vector space, thus might not be a valid differential type.
- For example, if the primal type is `DateTime`, its not a valid differential type as two.
+ For example, if the primal type is `DateTime`, its not a valid differential type as two
  `DateTime` can not be added (fun fact: `Milisecond` is a differential for `DateTime`).
 
-Where it is defined the operation of `extern` sfor a primal type `P` should be
+Where it is defined the operation of `extern` for a primal type `P` should be
 `extern(x) = zero(P) + x`.
 
-Because of its limitations, extern should only really be used for testing.
+Because of its limitations, `extern` should only really be used for testing.
 It can be useful, if you know what you are getting out, as it recursively removes thunks,
 and otherwise makes outputs more consistent with finite differencing.
-The more useful action in general to call `+`, or in the case of thunks: [`unthunk`](@ref).
+The more useful action in general is to call `+`, or in the case of thunks: [`unthunk`](@ref).
 
 Note that `extern` may return an alias (not necessarily a copy) to data
 wrapped by `x`, such that mutating `extern(x)` might mutate `x` itself.
