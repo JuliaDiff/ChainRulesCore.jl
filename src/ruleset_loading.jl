baf443180be5d823cdefa3cb97ded8915707af5e
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-08-21 12:50:51 +0100

    move comemnt

--- src/ruleset_loading.jl
+++ src/ruleset_loading.jl
@@ -1,5 +1,4 @@
 # Infastructure to support generating overloads from rules.
-
 function __init__()
     # Need to refresh rules when a package is loaded
     push!(Base.package_callbacks, pkgid -> refresh_rules())
@@ -52,16 +51,15 @@ If you previously wrong an incorrect hook, you can use this to get rid of the ol
 """
 clear_new_rule_hooks!(rule_kind) = empty!(_hook_list(rule_kind))
 
-
 """
     _rule_list(frule | rrule)
 
 Returns a list of all the methods of the currently defined rules of the given kind.
 Excluding the fallback rule that returns `nothing` for every input.
 """
+function _rule_list end
+# The fallback rules are the only rules defined in ChainRulesCore & that is how we skip them
 _rule_list(rule_kind) = (m for m in methods(rule_kind) if m.module != @__MODULE__)
-# ^ The fallback rules are the only rules defined in ChainRules core  so that is how we skip them.
-
 
 
 const LAST_REFRESH_RRULE = Ref(0)
