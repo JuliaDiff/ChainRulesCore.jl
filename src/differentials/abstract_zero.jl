35982e5c2cea3cd09c4f852ffaac56f2e2b507d0
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2020-11-20 18:35:04 +0000

    Add convert for AbstractZero

--- src/differentials/abstract_zero.jl
+++ src/differentials/abstract_zero.jl
@@ -24,6 +24,8 @@ Base.adjoint(z::AbstractZero) = z
 Base.transpose(z::AbstractZero) = z
 Base.:/(z::AbstractZero, ::Any) = z
 
+Base.convert(::Type{T}, x::AbstractZero) where T <: Real = zero(T)
+
 """
     Zero() <: AbstractZero
 
