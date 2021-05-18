15e206c471a9441c9a518b0ca5ecefb3aae601b5
Miha Zgubic <mzgubic@users.noreply.github.com>
2021-05-18 15:43:44 +0100

    Update src/deprecated.jl
    Co-authored-by: Nick Robinson <npr251@gmail.com>

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -2,5 +2,3 @@ Base.@deprecate_binding AbstractDifferential AbstractTangent
 Base.@deprecate_binding Composite Tangent
 Base.@deprecate_binding Zero ZeroTangent
 Base.@deprecate_binding DoesNotExist NoTangent
-Base.@deprecate_binding Thunk Thunk
-Base.@deprecate_binding InplaceableThunk InplaceableThunk
