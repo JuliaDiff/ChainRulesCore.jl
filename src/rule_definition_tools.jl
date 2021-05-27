08453d5cc939fa1030dadb050007f443080ff64b
Miha Zgubic <mzgubic@users.noreply.github.com>
2021-05-27 19:35:18 +0100

    Update src/rule_definition_tools.jl
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>

--- src/rule_definition_tools.jl
+++ src/rule_definition_tools.jl
@@ -56,7 +56,8 @@ derivative/setup expressions.
 This macro assumes complex functions are holomorphic. In general, for non-holomorphic
 functions, the `frule` and `rrule` must be defined manually.
 
-Prefer using `true` over `1.0` to express multiplicative identity.
+If the derivative is one, (e.g. for identity functions) `true` can be used as the most 
+general multiplicative identity.
 
 The `@setup` argument can be elided if no setup code is need. In other
 words:
