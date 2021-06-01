17bdcb99501c0376c064e0306e400482d8e704c8
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-06-01 21:49:43 +0100

    WIP: Initial sketch for RuleConfigs

---
+++ docs/src/config.md
@@ -0,0 +1,87 @@
+# [Rule configurations and calling back into AD](@id config)
+
+[`RuleConfig`](@ref) a method for making rules conditionally defined based on the presence of certain features in the AD system.
+Once key such feature is the ability to perform AD either in forwards or reverse mode or both.
+
+
+## Writing rules that call back into AD
+
+To define e.g. rules for higher order functions, it is useful to be able to call back into the AD system to get it to do some work for you.
+
+For example the rule for reverse mode AD for map might like to use forward mode AD if one is available.
+Particularly for the case where only a single input collection is being mapped over.
+In that case we know the most efficient way to compute that sub-program is in forwards, as each call with-in the map only takes a single input.
+
+Note: the following is not the most efficient rule for map via forward, but attempts to be clearer for demonstration purposes.
+
+```julia
+function rrule(config::RuleConfig{<:Function}, ::typeof(map), f::Function, x::Array{<:Real})
+    y_and_ḟ_and_ẏ = map(x) do xi
+        frule_via_ad(config, one(xi), f, xi)
+    end
+    ḟ_and_ẏ = last.(y_and_ẏ)
+
+    function pullback_map(x̄)
+        ḟ = first.(ḟ_and_ẏ)
+        ẏ = last.(ḟ_and_ẏ)
+        return NoTangent(), x̄ * sum(ḟ), x̄ .* ẏ
+    end
+
+    return first.(y_and_ḟ_and_ẏ)
+end
+```
+
+TODO demo for reverse.
+
+## Writing rules that depend on other special requirements of the AD.
+
+As of right now there are no such special properties defined.
+We add this now for future-proofing, as adding more type-parameters later is breaking.
+It is likely that in the future such will be provided for e.g. mutation support.
+
+Such a thing would look like:
+```julia
+struct SupportsMutation end
+
+function rrule(
+    ::RuleConfig{<:Any,<:Any, >:SupportsMutatation}, typeof(push!), x::Vector
+)
+    y = push!(x)
+
+    function push!_pullback(ȳ)
+        pop!(x)  # undo change to primal incase it is used in another pullback we haven't called yet
+        pop!(ȳ)  # accumulate gradient via mutating ȳ, then return ZeroTangent
+        return NoTangent(), ZeroTangent()
+    end
+
+    return y, push!_pullback
+end
+```
+and it would be used in the AD e.g. as follows:
+```julia
+struct EnzymeRuleConfig <: RuleConfig{Nothing,Nothing, Union{SupportsMutation}}
+```
+Note that in this case the `Union` is redudant since it has a single element.
+It is likely work keeping it, just so as to remember more are added by added them to the `Union`.
+
+
+
+
+
+Note: you can only depend on the presence of a feature, not it's absence.
+This means we may need to define features and there compliments, when one is not the obvious default.
+
+## Writing rules that are only for your own AD
+
+A special case of the above is writing rules that are defined only for your own AD.
+Rules which otherwise would be type-piracy, and would affect other AD systems.
+This could be done via making up a special property type and dispatching on it.
+But there is no need, as we can dispatch on the `RuleConfig` subtype directly.
+
+For example in order to avoid mutation in nested AD situations, Zygote might want to have a rule for [`add!!`](@ref) that makes it just do `+`.
+
+```julia
+struct ZygoteConfig <: RuleConfig{Nothing,Nothing, Union{}} end
+
+rrule(::ZygoteConfig, typeof(ChainRulesCore.add!!), a, b) = a+b, Δ->(NoTangent(), Δ, Δ)
+```
--- src/rules.jl
+++ src/rules.jl
@@ -1,5 +1,5 @@
 """
-    frule((Δf, Δx...), f, x...)
+    frule([::RuleConfig,] (Δf, Δx...), f, x...)
 
 Expressing the output of `f(x...)` as `Ω`, return the tuple:
 
@@ -50,15 +50,18 @@ So this is actually a [`Tangent`](@ref):
 ```jldoctest frule
 julia> Δsincosx
 Tangent{Tuple{Float64, Float64}}(0.6795498147167869, -0.7336293678134624)
-```.
+```
 
+The optional [`RuleConfig`](@ref) option allows specifying frules only for AD systems that
+support given features. If not needed, then it can be omitted and the `frule` without it
+will be hit as a fallback. This is the case for most rules.
 
-See also: [`rrule`](@ref), [`@scalar_rule`](@ref)
+See also: [`rrule`](@ref), [`@scalar_rule`](@ref), [`RuleConfig`](@ref)
 """
 frule(::Any, ::Vararg{Any}; kwargs...) = nothing
 
 """
-    rrule(f, x...)
+    rrule([::RuleConfig,] f, x...)
 
 Expressing `x` as the tuple `(x₁, x₂, ...)` and the output tuple of `f(x...)`
 as `Ω`, return the tuple:
@@ -101,7 +104,11 @@ julia> hypot_pullback(1) == (NoTangent(), (x / hypot(x, y)), (y / hypot(x, y)))
 true
 ```
 
-See also: [`frule`](@ref), [`@scalar_rule`](@ref)
+The optional [`RuleConfig`](@ref) option allows specifying rrules only for AD systems that
+support given features. If not needed, then it can be omitted and the `rrule` without it
+will be hit as a fallback. This is the case for most rules.
+
+See also: [`frule`](@ref), [`@scalar_rule`](@ref), [`RuleConfig`](@ref)
 """
 rrule(::Any, ::Vararg{Any}) = nothing
 
@@ -116,3 +123,55 @@ rrule(::Any, ::Vararg{Any}) = nothing
 # explicitly empty body to save the compiler that work.
 
 (::Core.kwftype(typeof(rrule)))(::Any, ::Any, ::Vararg{Any}) = nothing
+
+"""
+    RuleConfig{F, R, T}
+
+The configuration
+ - `F`: **frule-like**. This is singleton `typeof` a function which acts like `frule`, but
+   which functions via invoking an AD system. it must match the [`frule`](@ref) signature.
+   If you do not have such a function it must be set to `Nothing` instead.
+ - `R`: **rrule-like**. This is singleton `typeof` a function which acts like `rrule`, but
+   which functions via invoking an AD system. it must match the [`rrule`](@ref) signature.
+   If you do not have such a function it must be set to `Nothing` instead.
+ - `T`: **traits**. This should be a `Union` of all special traits needed for rules to be
+   allowed to be defined for your AD. If nothing special this should be set to `Union{}`.
+
+Rule authors can dispatch on this config when defining rules.
+For example:
+```julia
+# only define rrule for `pop!` on AD systems where mutation is supported.
+rrule(::RuleConfig{<:Any,<:Any,>:SupportsMutation}, typeof(pop!), ::Vector) = ...
+
+# this definition of map is for any AD that defines a forwards mode
+rrule(conf::RuleConfig{<:Function}, typeof(map), ::Vector) = ...
+
+# this definition of map is for any AD that only defines a reverse mode.
+# It is not as good as the rrule that can be used if the AD defines a forward-mode as well.
+rrule(conf::RuleConfig{Nothing,<:Function}, typeof(map), ::Vector) = ...
+```
+
+For more details see [rule configurations and calling back into AD](@ref config).
+"""
+abstract type RuleConfig{F<:Union{Function,Nothing}, R<:Union{Function,Nothing}, T} end
+
+# if no config is present then fallback to config-less rules
+frule(::RuleConfig, ārgs, f, args...; kwargs...) = frule(ārgs, f, args...; kwargs...))
+rrule(::RuleConfig, f, args...; kwargs...) = rrule(f, args...; kwargs...)
+
+function frule_via_ad(::RuleConfig{F}, ārgs, f, args...; kwargs...) where F <: Function
+    #TODO: Should this pass on the config? I suspect it should so we can use it for avoiding stack-overflows
+    return F.instance(ārgs, f, args...; kwargs...)
+end
+function rrule_via_ad(::RuleConfig{<:Any,R}, f, args...; kwargs...) where R <: Function
+    #TODO: Should this pass on the config? I suspect it should so we can use it for avoiding stack-overflows
+    return R.instance(ārgs, f, args...; kwargs...)
+end
+
+# TODO: do we want this? Or do we need to avoid ending up in a circumstance where it is needed
+function frule_via_ad(::RuleConfig{Nothing}, ārgs, f, args...; kwargs...) where F<: Function
+    return frule(ārgs, f, args...; kwargs...)
+end
+function rrule_via_ad(::RuleConfig{<:Any,Nothing}, f, args...; kwargs...)
+    return rrule(f, args...; kwargs...)
+end
