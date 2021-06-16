41d3a6d6826613c7e8aed220c777f30f6e8fba32
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-06-16 12:05:10 +0100

    code review

--- src/differentials/thunks.jl
+++ src/differentials/thunks.jl
@@ -8,11 +8,11 @@ Base.Broadcast.broadcastable(x::AbstractThunk) = broadcastable(unthunk(x))
     return element, (val, state)
 end
 
-@inline function Base.iterate(::AbstractThunk, (val, state))
-    next = iterate(val, state)
-    next isa Nothing && return nothing
+@inline function Base.iterate(::AbstractThunk, (underlying_object, state))
+    next = iterate(underlying_object, state)
+    next === nothing && return nothing
     element, new_state = next
-    return element, (val, new_state)
+    return element, (underlying_object, new_state)
 end
 
 Base.:(==)(a::AbstractThunk, b::AbstractThunk) = unthunk(a) == unthunk(b)
