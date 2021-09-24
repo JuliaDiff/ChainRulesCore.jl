fb53af7b6fe99407702c9c6c09853dda6c407ee9
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-09-24 18:55:26 +0100

    improve docstrings

--- src/ignore_gradients.jl
+++ src/ignore_gradients.jl
@@ -1,15 +1,39 @@
 """
+    ignore_gradients(f::Function)
+
+Tells the AD system to ignore the gradients of the wrapped closure. The primal computation
+(forward pass) is executed normally.
+
+```julia
+ignore_gradients() do
+    value = rand()
+    push!(collection, value)
+end
+```
+
+Using this incorrectly could lead to incorrect gradients. E.g.
+```julia
+function wrong_grads(x)
+    y = ones(3)
     ignore_gradients() do
-        ...
+        push!(y, x)
     end
-
-A convenience function that tells the AD system to ignore the gradients of the wrapped
-closure. The forward pass is executed normally.
-
-Using this incorrectly, for example by including calculations in the closure, could lead
-to incorrect gradients. A possible use case is logging of quantities in the forward pass.
+    return sum(y)
+end
+```
 """
 ignore_gradients(f::Function) = f()
+
+"""
+    ignore_gradients(x)
+
+Tells the AD system to ignore the gradients of the argument. Can be used to avoid
+unnecessary computation of gradients.
+
+```julia
+ignore_gradient(x) * w
+```
+"""
 ignore_gradients(x) = x
 
 @non_differentiable ignore_gradients(f)
