9cffbeaaf1d6ead15d11cc9d0728e8ff0f11014e
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-09-24 12:15:25 +0100

    use a macro, reword docstrings

--- src/utils.jl
+++ src/utils.jl
@@ -1,43 +1,25 @@
 """
-    ignore() do
+    ignore_gradients() do
         ...
     end
 
-Tell ChainRulesCore to ignore a block of code. Everything inside the `do`
-block will run on the forward pass as normal, but ChainRulesCore won't try to
-differentiate it at all. This can be useful for e.g. code that does logging of
-the forward pass.
+A convenience function that tells the AD system to ignore the gradients of the wrapped
+closure. The forward pass is executed normally.
 
-Obviously, you run the risk of incorrect gradients if you use this incorrectly
+Using this incorrectly, for example by including calculations in the closure, could lead
+to incorrect gradients. A possible use case is logging of quantities in the forward pass.
 """
-ignore(f) = f()
+ignore_gradients(f) = f()
+
+@non_differentiable ignore_gradients(f)
 
 """
-    @ignore (...)
+    @ignore_gradients (...)
 
-Tell ChainRulesCore to ignore an expression. Equivalent to
-`ignore() do (...) end`.
-Example:
-
-```julia-repl
-julia> f(x) = x
-julia> _, v_pullback = ChainRulesCore.rrule(ChainRulesCore.ignore, f)
-```
+Tell the AD system to ignore the expression. Equivalent to `ignore_gradients() do (...) end`.
 """
-
-function frule((_, ḟ), ::typeof(ignore), f)
-    return f(), Zero()
-end
-
-function rrule(::typeof(ignore), f)
-    function ignore_pullback(ȳ)
-        return (NO_FIELDS, Zero())
-    end
-    return f(), ignore_pullback
-end
-
-macro ignore(ex)
-    return :(ChainRulesCore.ignore() do 
+macro ignore_gradients(ex)
+    return :(ChainRulesCore.ignore_gradients() do
         $(esc(ex))
     end)
 end
