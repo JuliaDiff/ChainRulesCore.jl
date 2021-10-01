d9d046faeaec71e9922e31d8a3fa9dc7d5cbe510
David Widmann <david.widmann@it.uu.se>
2021-10-01 17:12:26 +0200

    Fix tests with Julia 1.0

--- test/utils.jl
+++ test/utils.jl
@@ -1,22 +1,23 @@
+# struct need to be defined outside of tests for julia 1.0 compat
+# custom complex number to test fallback definition
+struct CustomComplex{T}
+    re::T
+    im::T
+end
+
+Base.real(x::CustomComplex) = x.re
+Base.imag(x::CustomComplex) = x.im
+
+Base.conj(x::CustomComplex) = CustomComplex(x.re, -x.im)
+
+Base.:*(a::CustomComplex, b::Number) = CustomComplex(reim((a.re + a.im * im) * b)...)
+Base.:*(a::Number, b::CustomComplex) = b * a
+function Base.:*(a::CustomComplex, b::CustomComplex)
+    return CustomComplex(reim((a.re + a.im * im) * (b.re + b.im * im))...)
+end
+
 @testset "utils.jl" begin
     @testset "conjtimes" begin
-        # custom complex number to test fallback definition
-        struct CustomComplex{T}
-            re::T
-            im::T
-        end
-
-        Base.real(x::CustomComplex) = x.re
-        Base.imag(x::CustomComplex) = x.im
-
-        Base.conj(x::CustomComplex) = CustomComplex(x.re, -x.im)
-
-        Base.:*(a::CustomComplex, b::Number) = CustomComplex(reim((a.re + a.im * im) * b)...)
-        Base.:*(a::Number, b::CustomComplex) = b * a
-        function Base.:*(a::CustomComplex, b::CustomComplex)
-            return CustomComplex(reim((a.re + a.im * im) * (b.re + b.im * im))...)
-        end
-
         inputs = (randn(), randn(ComplexF64), CustomComplex(reim(randn(ComplexF64))...))
         for x in inputs, y in inputs
             @test realconjtimes(x, y) == real(conj(x) * y)
