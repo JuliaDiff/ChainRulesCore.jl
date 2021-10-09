6f4c1b4ae2d362d420e6170df76e319fae20311b
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-10-09 19:30:16 +0100

    set devbranch for docs to main

--- docs/make.jl
+++ docs/make.jl
@@ -67,4 +67,8 @@ makedocs(;
     checkdocs=:exports,
 )
 
-deploydocs(; repo="github.com/JuliaDiff/ChainRulesCore.jl.git", push_preview=true)
+deploydocs(;
+    repo="github.com/JuliaDiff/ChainRulesCore.jl.git",
+    devbranch="main",
+    push_preview=true
+)
