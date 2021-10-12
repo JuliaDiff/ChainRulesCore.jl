b4dbfe3c40db4e0950a497051a2b6cf8e8999f6e
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-10-12 10:44:44 -0400

    reshape Zero

--- test/tangent_types/abstract_zero.jl
+++ test/tangent_types/abstract_zero.jl
@@ -10,6 +10,9 @@
 
         @test sum(ZeroTangent()) == ZeroTangent()
         @test sum(NoTangent(); dims=2) == NoTangent()
+
+        @test reshape(NoTangent(), (1,:)) === NoTangent()
+        @test reshape(ZeroTangent(), 2,3) === ZeroTangent()
     end
 
     @testset "ZeroTangent" begin
