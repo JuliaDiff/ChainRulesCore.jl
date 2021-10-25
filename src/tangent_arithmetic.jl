774559ee7da1b800ed5d5d04e84e6d9474dac985
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-10-25 12:12:30 +0100

    implement -Tangent

--- src/tangent_arithmetic.jl
+++ src/tangent_arithmetic.jl
@@ -144,6 +144,8 @@ end
 Base.:+(a::Dict, d::Tangent{P}) where {P} = merge(+, a, backing(d))
 Base.:+(a::Tangent{P}, b::P) where {P} = b + a
 
+Base.:-(tangent::Tangent{P}) where {P} = map(x -> -x, tangent)
+
 # We intentionally do not define, `Base.*(::Tangent, ::Tangent)` as that is not meaningful
 # In general one doesn't have to represent multiplications of 2 differentials
 # Only of a differential and a scaling factor (generally `Real`)
