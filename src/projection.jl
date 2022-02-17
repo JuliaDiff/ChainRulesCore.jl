abd2e75ef3acb0ee6df498a7effb80cef7668268
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2022-02-17 13:56:06 +0000

    implement projection for inplaceablethunk

--- src/projection.jl
+++ src/projection.jl
@@ -122,6 +122,7 @@ ProjectTo(::Any) = identity
 
 # Thunks
 (project::ProjectTo)(dx::Thunk) = Thunk(project ∘ dx.f)
+(project::ProjectTo)(dx::InplaceableThunk) = Thunk(project ∘ dx.val.f)
 
 # Zero
 ProjectTo(::AbstractZero) = ProjectTo{NoTangent}()  # Any x::Zero in forward pass makes this one projector,
