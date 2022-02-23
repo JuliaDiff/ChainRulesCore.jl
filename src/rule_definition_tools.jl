e3362cb6fae7d01bd81a02ddf04d0402188f4a06
Miha Zgubic <mzgubic@users.noreply.github.com>
2022-02-23 14:41:21 +0000

    Make `@opt_out rrule(...)` automatically qualify `rrule` namespace as `ChainRulesCore.rrule` (#546)

--- src/rule_definition_tools.jl
+++ src/rule_definition_tools.jl
@@ -490,34 +490,45 @@ Similar applies for [`frule`](@ref) and [`ChainRulesCore.no_frule`](@ref)
 For more information see the [documentation on opting out of rules](@ref opt_out).
 """
 macro opt_out(expr)
-    no_rule_target = _no_rule_target_rewrite!(deepcopy(expr))
+    no_rule_target = _target_rewrite!(deepcopy(expr), true)
+    rule_target = _target_rewrite!(deepcopy(expr), false)
 
     return @strip_linenos quote
         $(esc(no_rule_target)) = nothing
-        $(esc(expr)) = nothing
+        $(esc(rule_target)) = nothing
     end
 end
 
-"Rewrite method sig Expr for `rrule` to be for `no_rrule`, and `frule` to be `no_frule`."
-function _no_rule_target_rewrite!(expr::Expr)
+"""
+    _target_rewrite!(expr::Expr, no_rule)
+
+Rewrite method sig `expr` for `rrule` to be for `no_rrule` or `ChainRulesCore.rrule`
+(with the CRC namespace qualification), depending on the `no_rule` argument.
+Does the equivalent for `frule`.
+"""
+function _target_rewrite!(expr::Expr, no_rule)
     length(expr.args) === 0 && error("Malformed method expression. $expr")
     if expr.head === :call || expr.head === :where
-        expr.args[1] = _no_rule_target_rewrite!(expr.args[1])
+        expr.args[1] = _target_rewrite!(expr.args[1], no_rule)
     elseif expr.head == :(.) && expr.args[1] == :ChainRulesCore
-        expr = _no_rule_target_rewrite!(expr.args[end])
+        expr = _target_rewrite!(expr.args[end], no_rule)
     else
         error("Malformed method expression. $(expr)")
     end
     return expr
 end
-_no_rule_target_rewrite!(qt::QuoteNode) = _no_rule_target_rewrite!(qt.value)
-function _no_rule_target_rewrite!(call_target::Symbol)
-    return if call_target == :rrule
-        :(ChainRulesCore.no_rrule)
-    elseif call_target == :frule
-        :(ChainRulesCore.no_frule)
+_target_rewrite!(qt::QuoteNode, no_rule) = _target_rewrite!(qt.value, no_rule)
+function _target_rewrite!(call_target::Symbol, no_rule)
+    return if call_target == :rrule && no_rule
+        :($ChainRulesCore.no_rrule)
+    elseif call_target == :rrule && !no_rule
+        :($ChainRulesCore.rrule)
+    elseif call_target == :frule && no_rule
+        :($ChainRulesCore.no_frule)
+    elseif call_target == :frule && !no_rule
+        :($ChainRulesCore.frule)
     else
-        error("Unexpected opt-out target. Exprected frule or rrule, got: $call_target")
+        error("Unexpected opt-out target. Expected frule or rrule, got: $call_target")
     end
 end
 
