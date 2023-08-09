7716844fb68f17cbef5fe3ba2a2f8db60cbf952e
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-08-09 12:55:27 +0200

    Update preferences.jl

--- src/preferences.jl
+++ src/preferences.jl
@@ -3,7 +3,7 @@ using Preferences
 const int2float_type = @load_preference("int2float", "Float64")
 
 function set_int2float!(int2float_type::String)
-    if !(int2float_type in ("Float64", "Float32"))
+    if !(int2float_type in ("Float64", "Float32", "Float16"))
         throw(ArgumentError("Invalid int2float type: \"$(int2float_type)\""))
     end
 
