4ad78b1889b7ca5c2fc7aaf3b4415073e9b8afc0
DomCRose <dominicdrose@gmail.com>
2023-09-04 13:49:34 +0100

    Add comment and issue link near separate edit

--- src/projection.jl
+++ src/projection.jl
@@ -227,7 +227,9 @@ function (project::ProjectTo{AbstractArray})(dx::AbstractArray{S,M}) where {S,M}
                 throw(_projection_mismatch(project.axes, size(dx)))
             end
         end
-        if dx isa AbstractGPUArray
+        # Reshape, copying to remove the wrapper if a GPUArray, see
+        # https://github.com/JuliaDiff/ChainRulesCore.jl/issues/624
+        if dx isa AbstractGPUArray 
             copy(reshape(dx, project.axes))
         else
             reshape(dx, project.axes)
