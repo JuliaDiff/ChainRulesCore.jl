e478e7fd201cb568d648d9658ed3656ad70fa672
Frames White <me@oxinabox.net>
2024-01-23 17:22:36 +0800

    docs more consistent

--- src/tangent_types/abstract_zero.jl
+++ src/tangent_types/abstract_zero.jl
@@ -100,7 +100,8 @@ For mutable composites types this is a structural [`MutableTangent`](@ref)
 For `Array`s, it is applied recursively for each element.
 For other types, in particular immutable types, we do not make promises beyond that it will be `iszero`
 and suitable for accumulating against.
-In general though, it is more likely to produce a structural tangent.
+For types without a tangent space (e.g. singleton structs) this returns `NoTangent()`.
+In general, it is more likely to produce a structural tangent.
 
 !!! warning Exprimental
     `zero_tangent`is an experimental feature, and is part of the mutation support featureset.
