a105ba930ed620bc547b5d14d933528537fec354
Frames White <me@oxinabox.net>
2024-02-08 18:35:32 +0800

    only test behavour with PersistentDict if it is defined

--- test/tangent_types/abstract_zero.jl
+++ test/tangent_types/abstract_zero.jl
@@ -184,13 +184,15 @@ end
         @test zero_tangent((1.0, 2.0)) == Tangent{Tuple{Float64,Float64}}(0.0, 0.0)
 
         @test ==(
-            zero_tangent(Dict{Int, Float64}(1=>2.4)),
+            zero_tangent(Dict{Int, Float64}(1 => 2.4)),
              Tangent{Dict{Int,Float64}}(Dict{Int, Float64}())
         )
-        @test ==(
-            zero_tangent(Base.PersistentDict(1=>2.4)),
-             Tangent{Base.PersistentDict{Int,Float64}}(Dict{Int, Float64}())
-        )
+        if isdefined(Base, :PersistentDict)
+            @test ==(
+                zero_tangent(Base.PersistentDict(1 => 2.4)),
+                Tangent{Base.PersistentDict{Int,Float64}}(Dict{Int, Float64}())
+            )
+        end
 
 
         # Higher order
